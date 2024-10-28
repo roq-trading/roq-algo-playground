e27034d9f8fde756d55adfefa6d180ed92802099
thraneh <thraneh@gmail.com>
2024-10-28 05:15:08 +0100

    chore

--- src/roq/algo/playground/test.py
+++ src/roq/algo/playground/test.py
@@ -14,12 +14,13 @@ def create_legs():
     )
     leg_2 = roq.algo.Leg(
         source=1,
-        account="A2",
+        account="A1",
         exchange="bybit",
-        symbol="BTC-PERPETUAL",
+        symbol="BTCUSD",
         time_in_force=roq.TimeInForce.GTC,
     )
-    return [leg_1, leg_2]
+    result = [leg_1, leg_2]
+    return result
 
 
 def create_strategy():
@@ -28,21 +29,21 @@ def create_strategy():
     parameters = (
         "max_age=10s;threshold=5;quantity_0=1;min_position_0=-5;max_position_0=5"
     )
-    strategy = roq.algo.Strategy(roq.algo.strategy.Type.ARBITRAGE, config, parameters)
-    return strategy
+    result = roq.algo.Strategy(roq.algo.strategy.Type.ARBITRAGE, config, parameters)
+    return result
 
 
 def create_reporter():
-    reporter = roq.algo.Reporter(type=roq.algo.reporter.Type.SUMMARY)
-    return reporter
+    result = roq.algo.Reporter(type=roq.algo.reporter.Type.SUMMARY)
+    return result
 
 
 def create_summary_reporter():
-    reporter = roq.algo.reporter.Summary(
+    result = roq.algo.reporter.Summary(
         market_data_source=roq.algo.MarketDataSource.TOP_OF_BOOK,
         sample_frequency=datetime.timedelta(seconds=60),
     )
-    return reporter
+    return result
 
 
 strategy = create_strategy()
