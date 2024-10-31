ba3189502e4a5857fa3d1fe1bfa6b20c1cb4c12e
thraneh <thraneh@gmail.com>
2024-10-31 16:00:40 +0100

    tools to extract data from algo reporter (EXPERIMENTAL)

--- src/roq/algo/playground/test.py
+++ src/roq/algo/playground/test.py
@@ -1,56 +1,106 @@
 #!/usr/bin/env python
 
+import os
 import datetime
+
+from types import SimpleNamespace
+
+import pandas as pd
+
 import roq
 
 
+CONFIG = SimpleNamespace(
+    strategy=SimpleNamespace(
+        type=roq.algo.strategy.Type.ARBITRAGE,
+        parameters="max_age=10s;threshold=5;quantity_0=1;min_position_0=-5;max_position_0=5",
+        strategy_id=123,
+        legs=[
+            dict(
+                source=0,
+                account="A1",
+                exchange="deribit",
+                symbol="BTC-PERPETUAL",
+                time_in_force=roq.TimeInForce.GTC,
+            ),
+            dict(
+                source=1,
+                account="A1",
+                exchange="bybit",
+                symbol="BTCUSD",
+            ),
+        ],
+    ),
+    reporter=SimpleNamespace(
+        type=roq.algo.reporter.Type.SUMMARY,
+        output_type=roq.algo.reporter.OutputType.JSON,
+    ),
+)
+
+
+PARAMS = [
+    "{HOME}/var/lib/roq/data/deribit-public.roq".format(**os.environ),
+    "{HOME}/var/lib/roq/data/bybit-public.roq".format(**os.environ),
+]
+
+
+####################################
+
+
+def create_settings():
+    result = roq.client.Settings2(
+        app={
+            "name": "trader",
+        },
+        loop={
+            "timer_freq": datetime.timedelta(milliseconds=100),
+        },
+        service={},
+        common={},
+    )
+    return result
+
+
 def create_legs():
-    leg_1 = roq.algo.Leg(
-        source=0,
-        account="A1",
-        exchange="deribit",
-        symbol="BTC-PERPETUAL",
-        time_in_force=roq.TimeInForce.GTC,
-    )
-    leg_2 = roq.algo.Leg(
-        source=1,
-        account="A1",
-        exchange="bybit",
-        symbol="BTCUSD",
-        time_in_force=roq.TimeInForce.GTC,
-    )
-    result = [leg_1, leg_2]
+    result = []
+    for item in CONFIG.strategy.legs:
+        leg = roq.algo.Leg(**item)
+        result.append(leg)
     return result
 
 
 def create_config():
     legs = create_legs()
-    result = roq.algo.strategy.Config(legs=legs, strategy_id=123)
+    result = roq.algo.strategy.Config(
+        legs=legs,
+        strategy_id=CONFIG.strategy.strategy_id,
+    )
     return result
 
 
 def create_strategy():
     config = create_config()
-    parameters = (
-        "max_age=10s;threshold=5;quantity_0=1;min_position_0=-5;max_position_0=5"
-    )
     result = roq.algo.strategy.create(
-        roq.algo.strategy.Type.ARBITRAGE, config, parameters
+        type=CONFIG.strategy.type,
+        config=config,
+        parameters=CONFIG.strategy.parameters,
     )
     return result
 
 
 def create_arbitrage_strategy():
     config = create_config()
-    parameters = (
-        "max_age=10s;threshold=5;quantity_0=1;min_position_0=-5;max_position_0=5"
+    result = roq.algo.arbitrage.create(
+        config=config,
+        parameters=CONFIG.strategy.parameters,
     )
-    result = roq.algo.arbitrage.create(config, parameters)
     return result
 
 
 def create_reporter():
-    result = roq.algo.reporter.create(type=roq.algo.reporter.Type.SUMMARY)
+    result = roq.algo.reporter.create(
+        type=CONFIG.reporter.type,
+    )
     return result
 
 
@@ -79,7 +129,38 @@ print(summary_reporter)
 # TODO: extract result data
 
 
-# XXX settings (latency)
-# XXX sources (settings + config)
+####################################
 
-reporter = roq.client.simulator2(create_reporter, create_matcher, create_strategy, config)
+
+settings = create_settings()
+print(settings)
+
+factory = dict(
+    create_strategy=lambda dispatcher, config: roq.algo.strateg.create(
+        type=CONFIG.strategy.type,
+        config=config,
+        parameters=CONFIG.strategy.parameters,
+    ),
+    create_reporter=lambda: roq.algo.reporter.create(
+        type=CONFIG.reporter.type,
+    ),
+    create_matcher=lambda dispatcher, source, exchange, symbol: None,
+)
+print(factory)
+
+config = create_config()
+print(config)
+
+reporter = roq.client.Simulator2.dispatch(
+    settings=settings,
+    factory=factory,
+    config=config,
+    parameters=PARAMS,
+)
+print(reporter)
+
+data = reporter.extract("history")
+print(data)
+
+df = pd.DataFrame(data)
+print(df)
